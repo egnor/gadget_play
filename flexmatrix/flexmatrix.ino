958f39e4fa8b0bc035d4a94106cae666076455de
egnor <egnor@ofb.net>
2021-07-26 10:35:49 -0700

    pretty lights

--- flexmatrix/flexmatrix.ino
+++ flexmatrix/flexmatrix.ino
@@ -1,29 +1,67 @@
+#include <math.h>
 #include <stdio.h>
 
+#include <Adafruit_MPR121.h>
 #include <FastLED.h>
+
+CRGB *pixel(CRGBArray<256> *leds, uint8_t x, uint8_t y) {
+  if ((y % 2) == 0) {
+    return &(*leds)[(15 - y) * 16 + 15 - x];
+  } else {
+    return &(*leds)[(15 - y) * 16 + x];
+  }
+}
 
 static uint32_t next_millis = 0;
 static uint16_t frame = 0;
 
-CRGBArray<256> leds;
+static CRGBArray<256> leds;
+
+static Adafruit_MPR121 touch;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("=== flexmatrix demo starting ===");
-  next_millis = millis();
 
   FastLED.addLeds<NEOPIXEL, 11>(leds, leds.len);
+
+  if (!touch.begin()) {
+    Serial.println("*** MPR121 init failed ***");
+    while (true) {}
+  }
+
+  next_millis = millis();
 }
 
 void loop() {
-  for (uint16_t i = 0 ; i < leds.len; ++i) {
-    uint16_t p = i + frame;
-    leds[i].setRGB(16 * (p % 16), p % 256, (p % 4096) / 16);
+  const uint16_t touched = touch.touched();
+
+  for (uint8_t y = 0 ; y < 16; ++y) {
+    for (uint8_t x = 0 ; x < 16; ++x) {
+      const float dx = x - 7.5f, dy = y - 7.5f;
+      const float r = sqrtf(dx * dx + dy * dy);
+      const float ang = atan2f(dy, dx);
+      const uint8_t hue = (ang + frame * 0.1f) * 128 / M_PI;
+      const uint8_t sat = min(255, r * 256 / 7.0f);
+      pixel(&leds, x, y)->setHSV(hue, sat, 255);
+    }
   }
+
+  // for (uint8_t v = 0; v < 16; ++v) {
+  //   pixel(&leds, v, 0)->setRGB(255, 0, 0);
+  //   pixel(&leds, 0, v)->setRGB(0, 255, 0);
+  // }
+  // pixel(&leds, 0, 0)->setRGB(255, 255, 255);
+
   FastLED.show();
 
   Serial.print("frame ");
-  Serial.println(frame);
+  Serial.print(frame);
+  Serial.print(" ");
+  for (uint8_t s = 0; s < 12; ++s) {
+    Serial.print(touched & (1 << s) ? "*" : ".");
+  }
+  Serial.println();
 
   ++frame;
   next_millis += 100;

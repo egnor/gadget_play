cf3f3dc8e596520e4e06a5229243781b85e66385
Dan Egnor <egnor@ofb.net>
2022-06-09 19:33:52 -0700

    start migrating to platformio

--- dw3000/dev_setup.py
+++ dw3000/dev_setup.py
@@ -2,13 +2,23 @@
 
 import os
 import signal
-import venv
 from pathlib import Path
 from subprocess import check_call, check_output
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 source_dir = Path(__file__).resolve().parent
 
+print("=== System packages (sudo apt install ...) ===")
+apt_packages = [
+    "cargo", "direnv", "libffi-dev", "libssl-dev",
+    "python3", "python3-dev", "python3-pip",
+]
+installed = check_output(["dpkg-query", "--show", "--showformat=${Package}\\n"])
+installed = installed.decode().split()
+if not all(p in installed for p in apt_packages):
+    check_call(["sudo", "apt", "install"] + apt_packages)
+
+import venv  # In case it just got installed above.
 print()
 print(f"=== Python packages (pip install ...) ===")
 venv_dir = source_dir / "build" / "python_venv"
@@ -18,8 +28,9 @@ if not venv_dir.is_dir():
     venv_dir.parent.mkdir(exist_ok=True, parents=True)
     venv.create(venv_dir, symlinks=True, with_pip=True)
 
+pips = ["platformio", "wheel"]
 check_call(["direnv", "allow", source_dir])
-check_call(["direnv", "exec", source_dir, "pip", "install", "pyserial"])
+check_call(["direnv", "exec", source_dir, "pip", "install", *pips])
 
 print()
 print("::: Setup complete :::")

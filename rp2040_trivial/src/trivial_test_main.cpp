d6ee346418bf00453f9036b5fa6e100d43215288
Dan Egnor <egnor@ofb.net>
2024-05-13 11:29:52 -0700

    trivial

--- rp2040_trivial/src/trivial_test_main.cpp
+++ rp2040_trivial/src/trivial_test_main.cpp
@@ -3,9 +3,13 @@
 void setup() {
   Serial.begin(115200);
   Serial.println("SETUP");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   Serial.println("LOOP");
-  delay(500);
+  delay(250);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);
+  digitalWrite(LED_BUILTIN, LOW);
 }
